7911115d5ff36fa3415daa085cec1cfa38bd1f27
twknab <20636750+twknab@users.noreply.github.com>
2018-01-30 04:58:13 -0800

    added built in functions info

---
+++ 01-fundamentals/07-built-in-functions.php
@@ -0,0 +1,44 @@
+<?php
+
+// These are some built in functions within PHP:
+
+/*
+Functions for Strings
+
+  strlen - get string length
+  strtolower - make a string lowercase
+  strtoupper - make a string uppercase
+  ucwords - uppercase the first character of each word in a string
+  str_replace - replace all occurrences of the search string with the replacement string
+  stristr - find the first occurrence of a string
+  strpos - find the position of the first occurrence of a substring in a string
+  explode - split a string
+  implode - joins arrays to a string
+
+Reference: http://php.net/manual/en/ref.strings.php
+
+Functions for Arrays
+
+  shuffle - shuffles the array
+  sort - sorts the array
+  count - counts the number of rows in the array
+  explode - breaks string into multiple values in the array
+  implode - joins information in the array as a string
+  array_pop - remove the last element in an array
+  array_push - adding elements into an array
+
+Reference: http://php.net/manual/en/ref.array.php
+
+Functions for Numbers
+
+  is_numeric(num) - returns true or false depending on whether or not the value is a number
+  rand(start, end) - generates a random number
+
+Additional Functions
+
+  isset(var) - returns true if var is set and is not NULL
+  empty(var) - Returns FALSE if var exists and has a non-empty, non-zero value. Otherwise returns TRUE.
+*/
+
+
+?>
