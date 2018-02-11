f8e1c270d6b1f0c1f7cc1af53f739b654b47fa5f
twknab <20636750+twknab@users.noreply.github.com>
2018-02-11 12:06:31 -0800

    removed unnecessary var from confirmation.php

--- 02-advanced/assignments/04-registration-without-db/confirmation.php
+++ 02-advanced/assignments/04-registration-without-db/confirmation.php
@@ -1,13 +1,9 @@
 <?php
 session_start();
-
 if (!isset($_SESSION["registered"])) {
   header("Location: index.php");
   die();
 }
-
-var_dump($_SESSION["files"]);
-var_dump($_SESSION["file_info"]);
 ?>
 <h1>Registration Confirmed!</h1>
 <h2>
