966047892f1ba10ebfdc15b049659739440a416f
twknab <20636750+twknab@users.noreply.github.com>
2018-02-11 12:37:25 -0800

    added php with mysql section for next lessons

---
+++ 03-php-with-mysql/01-connecting.php
@@ -0,0 +1,13 @@
+<?php
+/*
+A few notes: When we start MAMP we also run a MySQL server. We can use MySQL workbench to create or manipulate schemas, conneceting to our MySQL server. This is covered more in depth in the `learning-python` repository of mine.
+
+We can also use phpMyAdmin to manipulate our data, if we don't have MySQL Workbench installed (this is cool):
+
+Load in your browser: http://localhost:8888/phpMyAdmin
+
+As we proceed, just understand that MAMP creates a MySQL server, and we then either use MySQL Workbench, or phpMyAdmin to connect to our server, and create or manipulate schemas and entries. Note that databases when developing are stored locally on our machine, by MAMP. MySQL Workbench and phpMyAdmin are just tools for us to visually or syntactically, CRUD (create, read, update, destroy) schemas.
+*/
+
+
+?>
