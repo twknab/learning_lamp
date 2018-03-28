81210e72bad36b1c90de9e4278fad70645e93092
twknab <20636750+twknab@users.noreply.github.com>
2018-03-27 22:39:56 -0700

    added time display assgn

---
+++ 05-php-MVC/assignments/04-time-display/application/views/main.php
@@ -0,0 +1,33 @@
+<?php 
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>CodeIgniter Great Number Game</title>
+  <style>
+   body {
+     text-align: center;
+   }
+   .time {
+     width: 300px;
+     margin: 15px auto;
+     padding: 15px;
+     border: 2px solid silver;
+     font-size: 34px;
+     font-weight: bolder;
+     color: cornflowerblue;
+   }
+  </style>
+</head>
+<body>
+  <h3>The current date and time:</h3>
+  <div class="time">
+   <?=$time?>
+  </div>
+</body>
+</html>
